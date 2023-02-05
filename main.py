bd722dc400f734d21c9e39303974df789f0f8cb6
Eric <ericncnl3742@gmail.com>
2023-02-06 03:49:31 +0800

    add exceptions for input

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import time
 from downloader import NhentaiDownloader
 
 if __name__ == "__main__":
-    manga_id = input("Input ID : ")
-    process_count = int(input("Input process count : "))
+    while True:
+        try:
+            manga_id = int(input("Input ID : "))
+            break
+        except:
+            print("That's not a valid ID. Try again.")
+            continue
+    try:
+        process_count = int(input("Input process count (default is 30) : "))
+    except:
+        process_count = 30
     path = str(manga_id)
     start = time.time()
     NhentaiDownloader(manga_id, process_count, path=path)
